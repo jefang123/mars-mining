05489dc41fe8fc2d244c51b4e9fc32d08b17bc77
Jeff <jeffreyfang123@gmail.com>
2018-11-06 21:12:15 +0900

    Work on grid

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -20,12 +20,52 @@ class Grid extends React.PureComponent {
   componentDidMount() {
     // setInterval(this.props.fetchBots, 1000);
     // setInterval(this.props.fetchNodes, 1000);
-    this.props.fetchBots();
-    this.props.fetchNodes();
+    // this.props.fetchBots();
+    // this.props.fetchNodes();
   }
   render () {
+    if (!this.props.bots) {
+      return null
+    }
+    // let bots = this.props.bots
+    // let nodes = this.props.nodes 
+
+    // bots.forEach(bot => {
+    //   GRID[bot.X][bot.Y] = "blue"
+    // })
+
+    // nodes.forEach(node =>{
+    //   if (GRID[node.X][node.Y] === "blue") {
+    //     GRID[node.X][node.Y] = "purple"
+    //   } else {
+    //     GRID[node.X][node.Y] = "red"
+    //   }
+    // })
+
+    let array = []
+
+    let counter = 1;
+
+    for (var i = 0; i < GRID.length; i++) {
+      for (var j = 0; j < GRID[i].length; j++) {
+        if (GRID[i][j] === "blue") {
+          array.push(<div key={counter} className='blue'></div>)
+        }
+        else if (GRID[i][j] === "red" ) {
+          array.push(<div key={counter} className = 'red'></div>)
+        }
+        else if (GRID[i][j] === "purple" ) {
+          array.push(<div key={counter} className = 'purple'></div>)
+        }
+        else {
+          array.push(<div key={counter} className= 'white'></div>)
+        }
+        counter ++;
+      }
+    }
+
     return(
-    <p> Test </p>
+      array
     )
   }
 };
--- src/api_util.js
+++ src/api_util.js
@@ -1,41 +1,53 @@
+import $ from 'jquery';
+
 export const fetchBots= () => {
-  return sendGETRequest('/bots')
+  return $.ajax({
+    crossDomain: true,
+    method: 'GET',
+    dataType: 'jsonp',
+    url: 'headlight-tournament-4.herokuapp.com/bots'
+  });
 };
 
 export const fetchNodes = () => {
-  return sendGETRequest('/nodes')
+  return $.ajax({
+    rossDomain: true,
+    method: 'GET',
+    dataType: 'jsonp',
+    url: 'headlight-tournament-4.herokuapp.com/nodes'
+  });
 };
 
-function sendGETRequest(path, data, callback) {
+// function sendGETRequest(path, data, callback) {
 
-  const options = {
-    hostname: 'https://headlight-tournament-3.herokuapp.com',
-    port: 80,
-    path: path,
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+//   const options = {
+//     hostname: 'https://headlight-tournament-3.herokuapp.com',
+//     port: 80,
+//     path: path,
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   };
 
-  let response = [];
+//   let response = [];
 
-  var req = http.request(options, (res) => {
-    res.on('data', (chunk) => { response.push(chunk); });
-    res.on('end', () => { 
-      let finalResponse = JSON.parse(response.join(''));
-      callback(finalResponse); 
-    });
-  });
+//   req = http.request(options, (res) => {
+//     res.on('data', (chunk) => { response.push(chunk); });
+//     res.on('end', () => { 
+//       let finalResponse = JSON.parse(response.join(''));
+//       callback(finalResponse); 
+//     });
+//   });
 
-  req.write(JSON.stringify(data));
-  req.end();
-}
+//   req.write(JSON.stringify(data));
+//   req.end();
+// }
 
-// Sample request
+// // Sample request
 
-let cb = function(response) {
-  console.log("Here are all the bots: ", response);
-}
+// let cb = function(response) {
+//   console.log("Here are all the bots: ", response);
+// }
 
-sendGETRequest("/bots", cb);
+// sendGETRequest("/bots", cb);
